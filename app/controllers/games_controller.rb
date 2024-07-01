4e6836c545774faa6733697140282c0bb20920ca
Luke Kucinski <lukek22b@gmail.com>
2024-07-01 18:51:41 +0200

    longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,22 +1,30 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    @letters = ('a'..'z').to_a.sample(10).join
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
-  def included_letters?(answer, grid)
-    answer.downcase.chars.all? { |letter| answer.count(letter) <= grid.downcase.count(letter) }
-  end
-  def english_word?(answer)
-    "https://dictionary.lewagon.com/#{answer}"
-  end
+
   def score
-    if included_letters?(params[:answer], params[:grid])
-      if english_word?(params[:answer])
-        @response = "Congratulations! #{params[:answer]} is a valid English word!"
-      else
-        @response = "Sorry, but #{params[:answer]} doesn't seem to be a valid English word."
-      end
-    else
-      @response = "Sorry, but #{params[:answer]} can't be built out of #{params[:grid]}."
-    end
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
